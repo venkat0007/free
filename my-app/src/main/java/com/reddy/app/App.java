44cb59949e34379f96794c07d63b00b70cb692ed
venkat0007 <vennavenkat7@gmail.com>
2018-07-14 21:21:58 +0530

    commiting

---
+++ my-app/src/main/java/com/reddy/app/App.java
@@ -0,0 +1,13 @@
+package com.reddy.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
